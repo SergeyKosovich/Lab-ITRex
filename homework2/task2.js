bf855276de14b3d719bceb109ca083a7aae00308
SergeyKosovich <kosovichsergey322@gmail.com>
2021-07-26 15:36:48 +0300

    optimized code

--- homework2/task2.js
+++ homework2/task2.js
@@ -1,14 +1,11 @@
 function generateVolumes(arr, num) {
   const copyArr = arr.slice();
   function checkArray() {
-    const accum = [];
-    for (let index = 0; index < arr.length - 1; index += 1) {
-      accum.push({
-        index1: index,
-        index2: index + 1,
-        sum: +(copyArr[index] + copyArr[index + 1]),
-      });
-    }
+    const accum = copyArr.map((item, index) => ({
+      index1: index,
+      index2: index + 1,
+      sum: +(copyArr[index] + copyArr[index + 1]),
+    }));
     accum.sort((a, b) => a.sum - b.sum);
     const firstLowestInd = accum[0].index1;
     const sectLowestInd = accum[0].index2;
@@ -18,8 +15,7 @@ function generateVolumes(arr, num) {
   while (copyArr.length !== num) {
     checkArray();
   }
-  const maxTom = Math.max.apply(null, copyArr);
-  return maxTom;
+  return Math.max(...copyArr);
 }
 console.log(generateVolumes([1, 2, 1], 2));
 console.log(generateVolumes([1, 2, 1, 1], 3));
