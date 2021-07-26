e0772d4b84f8cddf321d513ec771f8c036448496
SergeyKosovich <kosovichsergey322@gmail.com>
2021-07-26 16:09:43 +0300

    fix task3 according to remarks

--- homework2/task3.js
+++ homework2/task3.js
@@ -1,36 +1,26 @@
 function copy(pages, xerox1, xerox2) {
-  let pagesCounter = pages;
-  const fastestXerox = Math.min(xerox1, xerox2);
-  const slowestXerox = Math.max(xerox1, xerox2);
-  const timeCycle = [];
-  let counter = 0;
+  const [fastestXerox, slowestXerox] = [xerox1, xerox2].sort((a, b) => a - b);
+  const timeCycle = new Array(Math.floor(slowestXerox / fastestXerox)).fill(fastestXerox);
   const difference = slowestXerox - Math.floor(slowestXerox / fastestXerox) * fastestXerox;
-  for (let i = 0; i < Math.floor(slowestXerox / fastestXerox); i += 1) {
-    timeCycle.push(fastestXerox);
-  }
   timeCycle.push(slowestXerox);
   // Прогоняем первую копию
-  pagesCounter -= 1;
-  counter += timeCycle[0];
+  let pagesCounter = pages - 1;
+  let counter = timeCycle[0];
   // Идем по порядку по timecycle пока не закончатся страницы
   while (pagesCounter !== 0) {
     for (let i = 0; i < timeCycle.length; i += 1) {
-      pagesCounter -= 1;
-      if (i === timeCycle.length - 1) {
-        if (pagesCounter === 0) {
-          counter += difference;
-        }
-      } else {
-        counter += timeCycle[i];
-      }
-      if (pagesCounter === 0) {
+      if (!pagesCounter) {
         break;
+      }
+      pagesCounter -= 1;
+      if (i === timeCycle.length - 1 && pagesCounter === 0) {
+        counter += difference;
+      } else if (i !== timeCycle.length - 1) {
+        counter += timeCycle[i];
       }
     }
   }
-  //
   return counter;
 }
-
 console.log(copy(4, 1, 1));
 console.log(copy(5, 1, 2));
