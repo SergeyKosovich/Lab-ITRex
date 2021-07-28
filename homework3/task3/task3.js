161c550033c0fe7e27dbf5386b66c2d28346904d
SergeyKosovich <kosovichsergey322@gmail.com>
2021-07-28 19:35:50 +0300

    fix task3

--- homework3/task3/task3.js
+++ homework3/task3/task3.js
@@ -1,18 +1,19 @@
 const fetch = require('node-fetch');
 
 class GetRandomUsers {
-  constructor(usersNum) {
-    this.reqUrl = `https://randomuser.me/api/?results=${usersNum}`;
+  constructor() {
+    this.reqUrl = 'https://randomuser.me/api/?results=';
   }
 
-  async getUsers() {
-    if (this.length !== 0) {
-      const a = await fetch(this.reqUrl).then((response) => response.json());
-      console.log(a.results);
+  async getUsers(num) {
+    if (num) {
+      const a = await fetch(this.reqUrl + num);
+      const data = await a.json();
+      console.log(data.results);
     } else {
       console.log('need add number of user ');
     }
   }
 }
-const getFiveUsers = new GetRandomUsers(3);
-getFiveUsers.getUsers();
+const getFiveUsers = new GetRandomUsers();
+getFiveUsers.getUsers(10);
